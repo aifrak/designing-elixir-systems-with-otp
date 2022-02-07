e5227de5f46f489e6ebd1c56451a6e8bbcffe4eb
aifrak <aifrak@users.noreply.github.com>
2022-02-07 11:07:38 +0000

    Add code from "Establish Your Boundaries"

--- counter/lib/counter.ex
+++ counter/lib/counter.ex
@@ -15,4 +15,20 @@ defmodule Counter do
   def hello do
     :world
   end
+
+  def start(initial_count) do
+    spawn(fn -> Counter.Server.run(initial_count) end)
+  end
+
+  def tick(pid) do
+    send(pid, {:tick, self()})
+  end
+
+  def state(pid) do
+    send(pid, {:state, self()})
+
+    receive do
+      {:count, value} -> value
+    end
+  end
 end

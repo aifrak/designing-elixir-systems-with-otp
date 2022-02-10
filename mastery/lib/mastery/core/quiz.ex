3a163e689ec705f8977c194331ca3dcc4749a91b
aifrak <aifrak@users.noreply.github.com>
2022-02-10 10:39:58 +0000

    Add code from "Choose a Random Question"

--- mastery/lib/mastery/core/quiz.ex
+++ mastery/lib/mastery/core/quiz.ex
@@ -29,4 +29,29 @@ defmodule Mastery.Core.Quiz do
 
   defp add_to_list_or_nil(nil, template), do: [template]
   defp add_to_list_or_nil(templates, template), do: [template | templates]
+
+  def select_question(%__MODULE__{templates: t}) when map_size(t) == 0, do: nil
+
+  def select_question(quiz) do
+    quiz
+    |> pick_current_question()
+    |> move_template(:used)
+    |> reset_template_cycle()
+  end
+
+  defp pick_current_question(quiz) do
+    Map.put(
+      quiz,
+      :current_question,
+      select_a_random_question(quiz)
+    )
+  end
+
+  defp select_a_random_question(quiz) do
+    quiz.templates
+    |> Enum.random()
+    |> elem(1)
+    |> Enum.random()
+    |> Question.new()
+  end
 end

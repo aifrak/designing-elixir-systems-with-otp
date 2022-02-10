736fbf356c43ddc49f7850e96a5ae698b5c06642
aifrak <aifrak@users.noreply.github.com>
2022-02-10 09:41:35 +0000

    Add code from "Name Concepts with Functions"

--- mastery/lib/mastery/core/question.ex
+++ mastery/lib/mastery/core/question.ex
@@ -3,6 +3,12 @@ defmodule Mastery.Core.Question do
 
   defstruct ~w[asked substitutions template]a
 
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
   defp build_substitution({name, choices_or_generator}) do
     {name, choose(choices_or_generator)}
   end
@@ -13,5 +19,19 @@ defmodule Mastery.Core.Question do
 
   defp choose(generator) when is_function(generator) do
     generator.()
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      substitutions: substitutions,
+      template: template
+    }
   end
 end
